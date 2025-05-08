94254c656d5fdafc4b7fd104c65eb5f72492d02b
Milovan Mihajlovic <mimamihajlovic@hotmail.com>
2025-05-08 17:41:00 +0200

    Run migrations only after prod deployment

--- Program.cs
+++ Program.cs
@@ -16,20 +16,24 @@ namespace StrategyGame
             builder.Services.AddControllers();
             builder.Services.AddDbContext<StrategyGameContext>(o => o.UseSqlServer(
                 builder.Configuration.GetConnectionString("DatabaseConnection")));
-            
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
 
+            //TODO: move this logic to Yaml pipeline
+
             using (var scope = app.Services.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetRequiredService<StrategyGameContext>();
-                
+
                 var pendingMigrations = dbContext.Database.GetPendingMigrations();
-                if (pendingMigrations.Any())
+                if (pendingMigrations.Any() && app.Environment.IsProduction()) 
                 {
                     Console.WriteLine("Applying pending migrations...");
                     dbContext.Database.Migrate();
