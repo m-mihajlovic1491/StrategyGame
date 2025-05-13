35c48e994c0cd6506dd8bac8990532ac4d06ff4c
Milovan Mihajlovic <mimamihajlovic@hotmail.com>
2025-05-13 11:53:15 +0200

    - get put and delete weapon endpoints

--- Controllers/WeaponController.cs
+++ Controllers/WeaponController.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using StrategyGame.Data;
+using StrategyGame.Extensions;
 using StrategyGame.Models;
 using StrategyGame.Requests;
 using StrategyGame.Validators;
@@ -8,6 +9,7 @@ using StrategyGame.Validators;
 namespace StrategyGame.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class WeaponController : ControllerBase
     {
         public StrategyGameContext _context { get; set; }
@@ -43,6 +45,60 @@ namespace StrategyGame.Controllers
             _context.SaveChanges();
 
             return Ok($"weapon {weapon.Name} saved to database");
+        }
+
+        [HttpGet]
+        [Route("Weapon/{id}")]
+
+        public IActionResult GetSingleWeapon([FromRoute] int id) 
+        {
+           var weapon = _context.Weapons.FirstOrDefault(x => x.Id == id);
+            if (weapon is null)
+            {
+                return NotFound($"weapon with id {id} not found");
+            }
+
+            return Ok(weapon.ToViewModel());
+        }
+
+        [HttpPut]
+        [Route("Weapon/{id}")]
+
+        public IActionResult UpdateSingleWeapon([FromRoute] int id, 
+            [FromBody] WeaponRequest request)
+        {
+            var weapon = _context.Weapons.FirstOrDefault(x => x.Id == id);
+
+            if (weapon is null)
+            {
+                return NotFound($"Weapon with id {id} not found");
+            }
+
+            weapon.Name = request.Name;
+            weapon.Damage = request.Damage;
+
+            _context.SaveChanges();
+            return Ok(weapon.ToViewModel());
+
+
+        }
+
+        [HttpDelete]
+        [Route("Weapon/{id}")]
+
+        public IActionResult DeleteWeapon([FromRoute] int id)
+        {
+           var weapon = _context.Weapons.FirstOrDefault(x => x.Id == id);
+
+            if (weapon is null)
+            {
+                return NotFound($"weapon with id {id} not found ");
+            }
+
+            _context.Weapons.Remove(weapon);
+            _context.SaveChanges();
+
+            return NotFound($"weapon {weapon.Name} with id {weapon.Id} deleted succefully ");
         }
 
     }
